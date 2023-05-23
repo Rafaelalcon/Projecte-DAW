e12a9930331ac3a18031b98465056bf7653205cf
rafael <rafael.alcon@estudiant.fjaverianas.com>
2023-05-23 11:53:48 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Projecte_DAW.Models;
+
+namespace Projecte_DAW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            const string apiUrl = "https://valorant-api.com/v1/agents";
+
+            var client = new HttpClient();
+            var response = client.GetAsync(apiUrl).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var model = JsonConvert.DeserializeObject<Root>(content);
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            const string Url = "https://valorant-api.com/v1/agents";
+            var client = new HttpClient();
+            var response = client.GetAsync(Url).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var model1 = JsonConvert.DeserializeObject<Root>(content);
+
+            return View(model1);
+        }
+
+        public IActionResult Index1()
+        {
+            const string Url2 = "https://valorant-api.com/v1/agents";
+            var client = new HttpClient();
+            var response = client.GetAsync(Url2).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var model2 = JsonConvert.DeserializeObject<Root>(content);
+
+            return View(model2);
+        }
+
+    }
+}
---
+++ Models/agentes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projecte_DAW.Models
+{
+    public class Ability
+    {
+        public string slot { get; set; }
+        public string displayName { get; set; }
+        public string description { get; set; }
+        public string displayIcon { get; set; }
+    }
+
+    public class Datum
+    {
+        public string uuid { get; set; }
+        public string displayName { get; set; }
+        public string description { get; set; }
+        public string developerName { get; set; }
+        public List<string> characterTags { get; set; }
+        public string displayIcon { get; set; }
+        public string displayIconSmall { get; set; }
+        public string bustPortrait { get; set; }
+        public string fullPortrait { get; set; }
+        public string fullPortraitV2 { get; set; }
+        public string killfeedPortrait { get; set; }
+        public string background { get; set; }
+        public List<string> backgroundGradientColors { get; set; }
+        public string assetPath { get; set; }
+        public bool isFullPortraitRightFacing { get; set; }
+        public bool isPlayableCharacter { get; set; }
+        public bool isAvailableForTest { get; set; }
+        public bool isBaseContent { get; set; }
+        public Role role { get; set; }
+        public List<Ability> abilities { get; set; }
+    }
+
+    public class Role
+    {
+        public string uuid { get; set; }
+        public string displayName { get; set; }
+        public string description { get; set; }
+        public string displayIcon { get; set; }
+        public string assetPath { get; set; }
+    }
+
+    public class Root
+    {
+        public int status { get; set; }
+        public List<Datum> data { get; set; }
+    }
+}
